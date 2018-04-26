631ade15c7927b9b517254d4f70270dd6c9b7ef3
andrewgwallace <andrew.g.wallace@gmail.com>
2018-04-26 16:28:33 -0400

    Add heroku production information

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,6 @@ module.exports = {
     test: {},
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_URL
+        connection:"postgres://dxikvlelusfknq:54da44c18fb65297acff293d9e2fda8b593000d0497cbf650e7c9117ca7b7293@ec2-54-225-200-15.compute-1.amazonaws.com:5432/da1kdqgbne8msj"
     }
 }
